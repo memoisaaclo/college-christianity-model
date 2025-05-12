ed255f548bbddc8d002f1360aee562c16c8beba0
Isaac Lo <isaaclo@iastate.edu>
2025-05-12 13:47:05 -0500

    fix graph 2 axes

--- generate_graph_two.py
+++ generate_graph_two.py
@@ -39,10 +39,11 @@ if __name__ == "__main__":
             [.3, .3, .25, .15]
         ]
 
-    betas = np.linspace(.01, 4, 100)
-    beta_rets = np.linspace(.01, 4, 100)
+    start, stop, steps = 0, 1, 50
+    betas = np.linspace(start, stop, steps)
+    beta_rets = np.linspace(start, stop, steps)
 
-    results = np.zeros((100, 100))
+    results = np.zeros((steps, steps))
     for i, beta in enumerate(betas):
         for j, beta_ret in enumerate(beta_rets):
             B = [[round(beta + B_step*(j - i), 3)
@@ -63,5 +64,19 @@ if __name__ == "__main__":
             C_percent = np.sum(model.results[-1][:, final_project_model.C_IDX])/4
             results[i][j] = C_percent
 
-    sb.heatmap(results, cmap='viridis')
+    results = np.flipud(results)
+
+    tick_positions = np.linspace(0, steps-1, 6)
+    tick_labels = np.round(np.linspace(start, stop, 6), 2)
+
+    ax = sb.heatmap(results, cmap='viridis', cbar_kws={'label': 'Final C Percentage'})
+    ax.set_xticks(tick_positions)
+    ax.set_xticklabels(tick_labels)
+    ax.set_yticks(tick_positions)
+    ax.set_yticklabels(np.flip(tick_labels))
+
+    ax.set_xlabel('Beta Ret (p_CS, p_DS)')
+    ax.set_ylabel('Beta (p_SC, p_SD)')
+    ax.set_title('Beta Values vs Final C Percent')
+
     plt.show()
